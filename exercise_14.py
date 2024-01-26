9ae2d30737b336faad928b134ecfcc6796cf1871
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 22:34:47 +0800

    description

---
+++ exercise_14.py
@@ -0,0 +1,12 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 14:
+# Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+# * * * * *  
+# * * * *  
+# * * *  
+# * *  
+# *
